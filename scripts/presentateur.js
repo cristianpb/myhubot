5fbe06abdb378c730175a78262f0382d1e9bbb6a
cristianpb <felipebrokate@gmail.com>
2018-08-31 09:01:12 +0200

    Test random selection

---
+++ scripts/presentateur.js
@@ -0,0 +1,34 @@
+module.exports = function(robot) {
+  return robot.hear(/test presentateur (.*)/i, function(res) {
+    let limit = res.match[1]
+    let mydict = {
+      'Mikael':0,
+      'Charlotte':0,
+      'Eelaman':0,
+      'Matthieu':0,
+      'Cristian':0,
+      'Stanislas':0
+    }
+    for (let n of fibonacci()) {
+      mydict[rand]++;
+      // truncate the sequence at 1000
+      if (n[0] >= limit) {
+        return res.send(`Results ${JSON.stringify(mydict)}`);
+        break;
+      }
+    }
+  });
+};
+
+function* fibonacci() { // a generator function
+  let prev = 0
+  while (true) {
+    myArray = ['Mikael', 'Charlotte', 'Eelaman', 'Matthieu',
+      'Cristian',
+      'Stanislas'
+    ]
+    rand = myArray[Math.floor(Math.random() * myArray.length)]
+    prev++;
+    yield [ prev, rand ];
+  }
+}
